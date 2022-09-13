83a49bf07f2a9f59e57d21d87acf16870b8eced8
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-12 22:04:03 -0400

    CharacterList

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Character from '../Character/Character';
 
 const CharacterList = () => {
+  const characterStuff = characters.map(character => (
+    <li key={character.id}>
+      <Character {...character}/>
+    </li>
+  ));
 
   return (
-
+    <ul>
+      {characterStuff}
+    </ul>
   );
 };
 
