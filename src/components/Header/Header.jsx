bd3dbf51f544fb7c72437e5c6219d2ccd862e560
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-13 20:24:31 -0400

    Header with toggle for state

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useCharacterToggle } from '../../hooks/characters';
+
+const Header = () => {
+  const toggle = useCharacterToggle();
+  return (
+    <header>
+      <div>
+        <button onClick={toggle()}>Toggle Character List</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
